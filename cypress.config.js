24c6aa56010f66758b08104e517bd9759ff445f5
Shaik Haneef <shaikhaneef82@gmail.com>
2024-01-10 10:53:47 +0530

    Support for screenshot and execution video in reports

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    reportPageTitle: "My Test Suite",
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on)
